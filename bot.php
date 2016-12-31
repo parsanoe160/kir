67e928cf2b154f72ac88611a25cc1fbf1ece9c7f
plugteam <parsanoe160@gmail.com>
2016-12-31 14:26:41 +0330

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@
 # if you need Help for develop this source , You Can Send Message To Me With @SpyGuard_BOT #
 ############################################################################################
 */
-define('API_KEY','BOTTOKEN');
+define('API_KEY','286084553:AAE611hrVTrRD6YG6vYE3EbTXyy7NNr6qRg');
 //----######------
 
 function makereq($method,$datas=[]){
@@ -78,7 +78,7 @@ $forward = $update->message->forward_from;
 
 
 
-$admin = 66443035;
+$admin = 222784754;
 //-------
 function SendMessage($ChatId, $TextMsg)
 {
